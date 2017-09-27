30ca148a2f1e35abb4373c22a58b0894f8542a93
Brice Allard <briceallard@gmail.com>
2017-09-27 00:45:28 -0500

    Added Dashboard
    Dashboard will display all up-to-date ship information. (NOT COMPLETE YET)

--- Programs/Asteroids/Source.cpp
+++ Programs/Asteroids/Source.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
+#include <stdlib.h>
 #include "asteroid.h"
 #include "ship.h"
+
+using namespace std;
 
 void fillVector(vector<Asteroid>&);
 // fillVector ==> gets Asteroid information from infile
@@ -13,14 +15,31 @@ void printVector(vector<Asteroid>&);
 // printVector ==> prints information about all asteroids
 // @param const vector<Asteroid>& ==> Asteroids in field
 
+void printDashboard();
+// printDashboard ==> prints header with ship information
+
 int main() {
 	vector<Asteroid> myAsteroids;
 
 	fillVector(myAsteroids);
 	printVector(myAsteroids);
+	system("pause");
+
+	printDashboard();
 
 	system("pause");
 	return 0;
+}
+
+// Just testing a menu style information header (NOT COMPLETE YET)
+void printDashboard() {
+	system("CLS");
+
+	cout << "======================================================\n"
+		<< "|            Ship Information Dashboard              |\n"
+		<< "| Position: [x,y]          Hull Weight(Tons): 12     |\n"
+		<< "| Health: 100%             Traveled (Miles): ~4.2k   |\n"
+		<< "======================================================\n";
 }
 
 void fillVector(vector<Asteroid>& newMyAsteroids) {
@@ -45,9 +64,9 @@ void printVector(vector<Asteroid>& myAsteroids) {
 	unsigned int size = myAsteroids.size();
 
 	for (unsigned int i = 0; i < size; i++) {
-		cout << "Asteroid " << i << "-" << endl;
+		cout << "Asteroid " << i + 1 << "- \n";
 		cout << "X: " << myAsteroids[i].getXCoord() << ' ';
 		cout << "Y: " << myAsteroids[i].getYCoord() << ' ';
-		cout << "Weight: " << myAsteroids[i].getWeight() << endl;
+		cout << "Weight: " << myAsteroids[i].getWeight() << '\n';
 	}
 }
