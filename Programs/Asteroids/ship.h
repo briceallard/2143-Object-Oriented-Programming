56fe204e57ca54c85d5ea7ce0c83071e0bbd5263
Brice Allard <briceallard@gmail.com>
2017-09-27 00:46:52 -0500

    .h File improved
    Added constructors/destructors and all accessor/mutator functions (that I could think of, more to come....)

--- Programs/Asteroids/ship.h
+++ Programs/Asteroids/ship.h
@@ -1,1 +1,44 @@
 #pragma once
+
+using namespace std;
+
+class Ship {
+/////////////////////////////////////////////////////
+public:
+	// Default constructor
+	Ship();
+
+	// Overloaded constructor
+	Ship(double, double);
+
+	//Destructor
+	~Ship();
+
+	// Accessor functions
+	double getCollectedWeight();
+	// getCollectedWeight
+	// @return double - total weight collected
+	double getTotalDistance();
+	// getTotalDistance
+	// @return double - total distance traveled
+	double getXCoord();
+	// getXCoord
+	// @return double - X coordinate for ship location
+	double getYCoord();
+	// getXCoord
+	// @return double - Y coordinate for ship location
+
+	// Mutator functions
+	void setXCoord(double);
+	void setYCoord(double);
+	void findAsteroid();
+	void collectAsteroid();
+/////////////////////////////////////////////////////
+private:
+	//Member variables
+	double totWeight;
+	double totDistance;
+	double shipXCoord;
+	double shipYCoord;
+	double shipHealth;
+};
