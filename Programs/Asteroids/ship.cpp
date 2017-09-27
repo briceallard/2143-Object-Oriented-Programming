0e3ad9ccabf9b7a67faa8c1c3a743831934cc53b
Brice Allard <briceallard@gmail.com>
2017-09-27 00:47:55 -0500

    Created ship.cpp
    Most function definitions created, but still needs to be finished.

---
+++ Programs/Asteroids/ship.cpp
@@ -0,0 +1,55 @@
+#include "ship.h"
+
+using namespace std;
+
+Ship::Ship() {
+	totWeight = 0;
+	totDistance = 0;
+	shipXCoord = 0;
+	shipYCoord = 0;
+	shipHealth = 100;
+}
+
+Ship::Ship(double x, double y) {
+	totWeight = 0;
+	totDistance = 0;
+	shipXCoord = x;
+	shipYCoord = y;
+	shipHealth = 100;
+}
+
+Ship::~Ship() {
+
+}
+
+double Ship::getCollectedWeight() {
+	return totWeight;
+}
+
+double Ship::getTotalDistance() {
+	return totDistance;
+}
+
+double Ship::getXCoord() {
+	return shipXCoord;
+}
+
+double Ship::getYCoord() {
+	return shipYCoord;
+}
+
+void Ship::setXCoord(double x) {
+	shipXCoord = x;
+}
+
+void Ship::setYCoord(double y) {
+	shipYCoord = y;
+}
+
+void Ship::findAsteroid() {
+
+}
+
+void Ship::collectAsteroid() {
+
+}
