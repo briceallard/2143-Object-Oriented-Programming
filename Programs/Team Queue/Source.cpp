93e4221a04644c474dee33b543607de4f51c41d8
Brice Allard <briceallard@gmail.com>
2017-11-28 01:41:50 -0600

    Update Source.cpp

--- Programs/Team Queue/Source.cpp
+++ Programs/Team Queue/Source.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <string>
+#include "Queue.h"
 
 using namespace std;
 
 int main() {
+	List<int> list;
+
+	list.enque(254, 2);
+	list.enque(163, 1);
+	list.enque(344, 3);
+	list.enque(255, 2);
+	list.deque();
+
+	list.print();
 
 	system("pause");
 	return 0;
