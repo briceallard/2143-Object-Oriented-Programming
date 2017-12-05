b75e238f8f9162cb8a5eee60df5e8cc271bbdb2f
Brice Allard <briceallard@gmail.com>
2017-12-04 20:47:16 -0600

    Final Queue.h
    Finished queue and added comments. All complete!

--- Programs/Team Queue/Queue.h
+++ Programs/Team Queue/Queue.h
@@ -1,5 +1,18 @@
+/*
+Brice Allard
+Program 5 - Team Queue
+Dr. Stringfellow
+
+Header file for my queue class.
+
+**NOTES: enqueue is default for standard
+	queues, however for the team queue in
+	this program we will be using insert
+*/
+
 #pragma once
 
+// Struct that will hold team data in Node
 template <class T>
 struct Node {
 	T data;
@@ -10,6 +23,7 @@ struct Node {
 
 template <class T>
 class List {
+	// Don't want to type <T> anymore
 	typedef Node<T> Node;
 
 public:
@@ -23,7 +37,7 @@ public:
 	// Mutator Functions
 	void enque(T, int);
 	void insert(T, int);
-	void deque();
+	int deque();
 	void deleteAll();
 	
 	// Accessor Functions
@@ -31,6 +45,7 @@ public:
 	void print();
 
 private:
+	// Member variables
 	Node *head;
 	Node *tail;
 };
